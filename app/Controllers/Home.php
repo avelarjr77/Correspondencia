3e4797697e8a5b052861eacee491c51510fb5c3d
Rebeca <kattantoledo@gmail.com>
2022-04-23 20:33:21 -0600

    CRUD Etapa

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,6 +1,4 @@
-<?php  
-
-namespace App\Controllers;
+<?php  namespace App\Controllers;
 
 use App\Models\Usuarios;
 
@@ -15,16 +13,18 @@ class Home extends BaseController
     public function login(){
         //dd($this->request->getPost());
         $usuario = trim($this->request->getVar('usuario'));
-        $clave = trim($this->request->getVar('clave'));
+        $clave = $this->request->getVar('clave');
 
         $usuarios = model('Usuarios');
+        $pass=$usuarios->obtenerUsuario('clave',$clave);
 
         if(!$user=$usuarios->obtenerUsuario('usuario',$usuario)){
 
             return redirect()->to(base_url('/'))->with('mensaje','0');
 
         }
-        if(!password_verify($clave, $user['clave'])){
+
+        if(isset($pass['clave'])){
 
             return redirect()->to(base_url('/home'))->with('mensaje','1');
 
---
+++ app/Models/modProceso/EtapaModel.php
@@ -0,0 +1,49 @@
+<?php 
+namespace App\Models\modProceso;
+
+use CodeIgniter\Model;
+
+class EtapaModel extends Model{
+
+    //MODELO PARA LISTAR PROCESO
+    public function listarEtapa()
+    {
+        $etapa =  $this->db->query("SELECT e.etapaId as 'id', e.nombreEtapa as 'nombre',e.orden as 'orden', p.nombreProceso as 'proceso'
+                                      FROM  wk_etapa e 
+                                      INNER JOIN wk_proceso p ON e.procesoId = p.procesoId
+                                      ORDER BY e.etapaId");
+        return $etapa->getResult();
+    }
+    
+    //MODELO PARA LISTAR TIPO PROCESO
+    public function listarProceso()
+    {
+        $proceso =  $this->db->query('SELECT*FROM  wk_proceso');
+        return $proceso->getResult();
+    }
+
+    //MODELO PARA AGREGAR PROCESO
+    public function insertar($datos){
+
+        $nombre = $this->db->table('wk_etapa');
+        $nombre->insert($datos);
+
+        return $this->db->insertID();
+    }
+
+    //MODELO PARA ELIMINAR PROCESO
+    public function eliminar($data){
+        $nombres = $this->db->table('wk_etapa');
+        $nombres->where($data);
+        
+        return $nombres->delete();
+    }
+
+    //Edita el registro en PROCESO
+    public function actualizar($data, $etapaId){
+        $nombres = $this->db->table('wk_etapa');
+        $nombres->set($data);
+        $nombres->where('etapaId', $etapaId);
+        return $nombres->update();
+    }
+}
