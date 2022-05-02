b721ba3ef718d506b832e191d823df0cb576c262
shelimber <Shelimber2012@gmail.com>
2022-05-01 20:25:40 -0600

    prueba

--- app/Views/template/admin_template.php
+++ app/Views/template/admin_template.php
@@ -48,7 +48,7 @@
               </div>
               <div class="profile_info">
                 <span>Bienvenido,</span>
-                <h2>John Doe</h2>
+                <h2><?php echo session('usuario') ?></h2>
               </div>
             </div>
             <!-- /menu profile quick info -->
