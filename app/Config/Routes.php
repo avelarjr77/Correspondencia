779b0321a14ce685760bb46d1f837b398a677109
shelimber <Shelimber2012@gmail.com>
2022-05-11 22:18:25 -0600

    Filters

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,9 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Login::index');
+
+                                    //['filter'=> 'auth']
+$routes->get('/', 'Login::index' ) ;
 $routes->post('/home', 'Home::login', ['as'=> 'home']);
 $routes->get('/salir', 'Home::salir');
 
