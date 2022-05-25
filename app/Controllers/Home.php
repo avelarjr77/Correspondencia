4e6bce1a3d476c4d860f8240ed9a73c9ac9d1774
avelarjr77 <oscar-avelarje@hotmail.com>
2022-05-25 12:22:14 -0600

    Actualizacion Home y Routes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -28,15 +28,14 @@ class Home extends BaseController
 
         #Seleccionar el rolId de l usuario logueado
         /* $rolId = $this->db->query("SELECT rolId FROM wk_usuario WHERE usuario='Mar97' LIMIT 1");*/
-<<<<<<< HEAD
+
         print_r($usuarios);
 
         if ($user = $usuarios->obtenerUsuario('usuario', $usuario) && isset($pass['clave'])) {
-=======
+
         print_r($usuarios); 
 
         if($user=$usuarios->obtenerUsuario('usuario',$usuario) && isset($pass['clave'])){
->>>>>>> 6e7b0dce91127995cfa0abb597c8d1eb3fdcb587
 
             $data = array(
                 'usuario' => $usuario,
@@ -46,16 +45,10 @@ class Home extends BaseController
             $session = session();
             $session->set($data);
 
-<<<<<<< HEAD
             return redirect()->to(base_url('/home'))->with('mensaje', '0');
-        } else {
-            return redirect()->to(base_url('/'))->with('mensaje', '1');
-=======
-            return redirect()->to(base_url('/home'))->with('mensaje','0');
 
         }else{
             return redirect()->to(base_url('/'))->with('mensaje','1');
->>>>>>> 6e7b0dce91127995cfa0abb597c8d1eb3fdcb587
         }
     }
 
@@ -81,3 +74,4 @@ class Home extends BaseController
         return redirect()->to(base_url('/'));
     }
 }
+?>
