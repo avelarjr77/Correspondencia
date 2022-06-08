a32d03d701f068df316490e97671a20f3adecede
Oscar Avelar <50642576+avelarjr77@users.noreply.github.com>
2022-06-07 21:11:59 -0600

    Update templateModulo.php

--- app/Views/template/templateModulo.php
+++ app/Views/template/templateModulo.php
@@ -149,24 +149,16 @@
       </div>
       <!-- /top navigation -->
 
-      <!-- sidebar menu -->
-      <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-        <div class="menu_section">
-        </div>
+
+
+      <!-- page content -->
+      <div class="right_col" role="main">
+        <?= $this->renderSection('content'); ?>
       </div>
+      <!-- /page content -->
 
-
-    </div>
-    <!-- /sidebar menu -->
-
-    <!-- page content -->
-    <div class="right_col" role="main">
-      <?= $this->renderSection('content'); ?>
-    </div>
-    <!-- /page content -->
-
-    <!-- footer content -->
-    <footer>
+      <!-- footer content -->
+      <footer>
         <div class="text-center">
           Sistema de Correspondencia &copy; <?= date('Y'); ?>
           <br>
@@ -174,61 +166,57 @@
         </div>
         <div class="clearfix"></div>
       </footer>
-    <!-- /footer content -->
+      <!-- /footer content -->
+    </div>
   </div>
-
-
-  <!-- jQuery -->
-  <script src="vendors/jquery/dist/jquery.min.js"></script>
-  <!-- Bootstrap -->
-  <script src="vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <!-- FastClick -->
-  <script src="vendors/fastclick/lib/fastclick.js"></script>
-  <!-- NProgress -->
-  <script src="vendors/nprogress/nprogress.js"></script>
-  <!-- Chart.js -->
-  <script src="vendors/Chart.js/dist/Chart.min.js"></script>
-  <!-- gauge.js -->
-  <script src="vendors/gauge.js/dist/gauge.min.js"></script>
-  <!-- bootstrap-progressbar -->
-  <script src="vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-  <!-- iCheck -->
-  <script src="vendors/iCheck/icheck.min.js"></script>
-  <!-- Skycons -->
-  <script src="vendors/skycons/skycons.js"></script>
-  <!-- Flot -->
-  <script src="vendors/Flot/jquery.flot.js"></script>
-  <script src="vendors/Flot/jquery.flot.pie.js"></script>
-  <script src="vendors/Flot/jquery.flot.time.js"></script>
-  <script src="vendors/Flot/jquery.flot.stack.js"></script>
-  <script src="vendors/Flot/jquery.flot.resize.js"></script>
-  <!-- Flot plugins -->
-  <script src="vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-  <script src="vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-  <script src="vendors/flot.curvedlines/curvedLines.js"></script>
-  <!-- DateJS -->
-  <script src="vendors/DateJS/build/date.js"></script>
-  <!-- JQVMap -->
-  <script src="vendors/jqvmap/dist/jquery.vmap.js"></script>
-  <script src="vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-  <script src="vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
-  <!-- bootstrap-daterangepicker -->
-  <script src="vendors/moment/min/moment.min.js"></script>
-  <script src="vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
-
-  <!-- Custom Theme Scripts -->
-  <script src="build/js/custom.min.js"></script>
-
-  <!--SweetAlert-->
-  <script src="vendors/sweetalert2/sweetalert2.min.js"></script>
-  <script src="vendors/sweetalert2/sweetalert.min.js"></script>
-
-  <!-- HOLA SOY UNA PRUEBA-->
-
-  <!--<script src="vendors/popper/umd/popper.min.js"></script>-->
-
-
-
 </body>
+
+
+<!-- jQuery -->
+<script src="vendors/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!-- FastClick -->
+<script src="vendors/fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="vendors/nprogress/nprogress.js"></script>
+<!-- Chart.js -->
+<script src="vendors/Chart.js/dist/Chart.min.js"></script>
+<!-- gauge.js -->
+<script src="vendors/gauge.js/dist/gauge.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<!-- iCheck -->
+<script src="vendors/iCheck/icheck.min.js"></script>
+<!-- Skycons -->
+<script src="vendors/skycons/skycons.js"></script>
+<!-- Flot -->
+<script src="vendors/Flot/jquery.flot.js"></script>
+<script src="vendors/Flot/jquery.flot.pie.js"></script>
+<script src="vendors/Flot/jquery.flot.time.js"></script>
+<script src="vendors/Flot/jquery.flot.stack.js"></script>
+<script src="vendors/Flot/jquery.flot.resize.js"></script>
+<!-- Flot plugins -->
+<script src="vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="vendors/flot.curvedlines/curvedLines.js"></script>
+<!-- DateJS -->
+<script src="vendors/DateJS/build/date.js"></script>
+<!-- JQVMap -->
+<script src="vendors/jqvmap/dist/jquery.vmap.js"></script>
+<script src="vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="vendors/moment/min/moment.min.js"></script>
+<script src="vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="build/js/custom.min.js"></script>
+
+<!--SweetAlert-->
+<script src="vendors/sweetalert2/sweetalert2.min.js"></script>
+<script src="vendors/sweetalert2/sweetalert.min.js"></script>
+
+
 
 </html>
