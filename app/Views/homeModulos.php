f4b45133b595a50867ff4ac78b53ff6c863af08c
Oscar Avelar <50642576+avelarjr77@users.noreply.github.com>
2022-06-15 14:13:58 -0600

    Update homeModulos.php

--- app/Views/homeModulos.php
+++ app/Views/homeModulos.php
@@ -2,7 +2,6 @@
 <?= $this->section('content') ?>
 
 <!-- page content modulos -->
-
 <div class="row">
     <div class="x_panel">
         <div class="x_content">
@@ -11,31 +10,29 @@
 
             <div class="clearfix"></div>
             <?php foreach ($modulo as $key) : ?>
-            <div class="col-md-4 col-sm-4  profile_details">
-                <div class="well profile_view">
-                    <div class="col-sm-12">
-                        <h4 class="brief"><i><?= $key->modulo ?></i></h4>
-                        <div class="left col-sm-7">
-                            <h2>
-                                <?= $key->descripcion ?>
-                            </h2>
+                <div class="col-md-4 col-sm-4  profile_details">
+                    <div class="well profile_view">
+                        <div class="col-sm-12">
+                            <h4 class="brief"><i><?= $key->modulo ?></i></h4>
+                            <div class="left col-sm-7">
+                                <h2>
+                                    <?= $key->descripcion ?>
+                                </h2>
                             </div>
-                        <div class="right col-sm-5 text-center">
-                        <div class="icon"><i class=" <?php echo $key->icono ?>" style="font-size: 75px;"></i></div><br>
-                        <a  type="button" class="btn btn-primary btn-sm" href="<?= base_url() . route_to('homeMenus') ?>"
-                        data-href="<?php echo base_url() . '/template/admin_template' . $key->moduloId ?>"
-                        data-nombre="<?php echo $key->modulo ?>">
-                        Ir al modulo
-                        </a>
+                            <div class="right col-sm-5 text-center">
+                                <div class="icon"><i class=" <?php echo $key->icono ?>" style="font-size: 75px;"></i></div><br>
+                                <form id="moduloId"  action="<?php echo base_url() . '/homeMenus'?>"  method="POST">
+                                    <button type="submit" class="btn btn-primary btn-sm">
+                                        Ir al modulo
+                                    </button>
+                                    <input type="hidden" name="moduloId" class="moduloId" id="moduloId" value="<?= $key->moduloId ?> ">
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             <?php endforeach; ?>
         </div>
     </div>
 </div>
-
-<!-- /page content modulos -->
-
 <?= $this->endSection() ?>
