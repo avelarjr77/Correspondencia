aa924dd43d32334280da83e05fe490bd8930c12e
Oscar Avelar <50642576+avelarjr77@users.noreply.github.com>
2022-06-15 14:17:41 -0600

    Update HomeMenusController.php

--- app/Controllers/HomeMenusController.php
+++ app/Controllers/HomeMenusController.php
@@ -3,14 +3,38 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\modAdministracion\ModuloModel;
 
 class HomeMenusController extends BaseController
 {
     //Funcion para MOSTRAR DATOS DE LA TABLA MENU
     public function index()
     {
+        $session = \Config\Services::session();
+        $modulo = $_POST['moduloId'];
+        //para obtener el Id de modulo por medio de session
+        $dataM=[
+            "modulo" => $modulo,
+            'is_logged' => true,
+        ];
+        $session = session();
 
-        return view('homeMenus');
+        $session->set($dataM);
+
+        $mod = new ModuloModel();
+        $data=[
+            'title'	=> 'Correspondencia UCAD | Home',
+            ];
+        return view('homeMenus', $data);
+        return view ('/template/admin_template', $dataM);
+    }
+    public function homeMenu()
+    {
+
+        $data=[
+            'title'	=> 'Correspondencia UCAD | Home',
+            ];
+        return view('homeMenus', $data);
     }
 
 }
