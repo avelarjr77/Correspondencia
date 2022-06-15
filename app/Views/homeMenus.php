28646b9c59572dd9f3f7682d022e983cb2134602
Oscar Avelar <50642576+avelarjr77@users.noreply.github.com>
2022-06-15 14:14:34 -0600

    Update homeMenus.php

--- app/Views/homeMenus.php
+++ app/Views/homeMenus.php
@@ -3,7 +3,6 @@
 
 <div class="row"><br>
     <h6><a href="<?= base_url() . route_to('homeModulos') ?>">Modulos</a></h6>
-    <h6><a href="<?= base_url() . route_to('homeMenus') ?>">/Configuración General</a></h6>
 </div>
 <h3>Home</h3>
 
