bf91a1dc27df423a346f2e95ac0f9b2a8ba8bc80
Oscar Avelar <50642576+avelarjr77@users.noreply.github.com>
2022-06-16 16:53:31 -0600

    Update homeModulos.php

--- app/Views/homeModulos.php
+++ app/Views/homeModulos.php
@@ -3,6 +3,22 @@
 
 <!-- page content modulos -->
 <div class="row">
+    <!-- Verificar correo y usuario -->
+    <div class="container">
+        <div class="col-md-9"></div>
+        <div class="col-md-3" style="z-index: 300;">
+
+            <?php if (session()->get('success')) : ?>
+                <div class="alert alert-info alert-dismissible fade show" role="alert">
+                    <i class="fa fa-user"></i>
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <?= session()->get('success') ?>
+                </div>
+            <?php endif; ?>
+
+
+        </div>
+    </div>
     <div class="x_panel">
         <div class="x_content">
             <div class="col-md-12 col-sm-12  text-center">
@@ -21,7 +37,7 @@
                             </div>
                             <div class="right col-sm-5 text-center">
                                 <div class="icon"><i class=" <?php echo $key->icono ?>" style="font-size: 75px;"></i></div><br>
-                                <form id="moduloId"  action="<?php echo base_url() . '/homeMenus'?>"  method="POST">
+                                <form id="moduloId" action="<?php echo base_url() . '/homeMenus' ?>" method="POST">
                                     <button type="submit" class="btn btn-primary btn-sm">
                                         Ir al modulo
                                     </button>
@@ -35,4 +51,11 @@
         </div>
     </div>
 </div>
+<script>
+    window.setTimeout(function() {
+        $(".alert").fadeTo(300, 0).slideUp(300, function() {
+            $(this).remove();
+        });
+    }, 5000);
+</script>
 <?= $this->endSection() ?>
