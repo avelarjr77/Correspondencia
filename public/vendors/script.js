13832457f25bd89494aaf6e8f6b3e668c28ab522
Oscar Avelar <50642576+avelarjr77@users.noreply.github.com>
2022-11-17 19:45:44 -0600

    Create script.js

---
+++ public/vendors/script.js
@@ -0,0 +1,8 @@
+$("#nombreDocumento").fileinput({
+    language: 'es',
+    maxFilePreviewSize: 21000,
+    maxFileSize: 21000,
+    actionUpload: false,
+    showZoom: true,
+    initialPreviewAsData: true,
+});
